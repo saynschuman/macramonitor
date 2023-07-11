78a3ec54dc1b586c3e96134a5d80bd4044bc4f89
vitaliy <vitaliy@heliotrope.digital>
2023-07-11 17:03:58 +0300

    first step

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { MantineProvider, Text } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
+import { HeaderTabs } from './HeaderTabs';
 
 export default function App() {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{ primaryColor: 'teal' }}>
+      <HeaderTabs
+        tabs={['Financial', 'PI', 'Quality', 'CPIA', 'Data', 'Action Items', 'Submission', 'Admin']}
+        links={[
+          { label: 'About', link: '1' },
+          { label: 'MacraGrur', link: '2' },
+          { label: 'Logout', link: '3' },
+        ]}
+      />
     </MantineProvider>
   );
 }
