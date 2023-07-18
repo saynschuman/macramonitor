9e53b78340b1abe33ca3e88aa345ba650948974b
vitaliy <vitaliy@heliotrope.digital>
2023-07-18 14:01:36 +0300

    select added

---
+++ src/components/MultiLevelSelect.tsx
@@ -0,0 +1,55 @@
+import { Button, Group, Text, Collapse, Box } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconCaretDown } from '@tabler/icons-react';
+import { Accordion } from '@mantine/core';
+import { useState } from 'react';
+
+export function MultiLevelSelect() {
+  const [opened, { toggle }] = useDisclosure(false);
+  const [value, setValue] = useState<string | null>(null);
+
+  const selectValue = (v: string) => {
+    setValue(v);
+    toggle();
+  };
+
+  return (
+    <Box maw={400} mx="auto">
+      <Group position="right" mb={5}>
+        <Button variant="outline" onClick={toggle} rightIcon={<IconCaretDown />}>
+          {value ?? 'Choose value'}
+        </Button>
+      </Group>
+
+      <Collapse in={opened}>
+        <Text>
+          <Accordion radius="md">
+            <Accordion.Item value="1">
+              <Accordion.Control>Item 1</Accordion.Control>
+              <Box sx={{ paddingLeft: 10 }}>
+                <Accordion.Panel
+                  sx={{ cursor: 'pointer' }}
+                  onClick={() => selectValue('Sub item 1')}
+                >
+                  Sub item 1
+                </Accordion.Panel>
+                <Accordion.Panel
+                  sx={{ cursor: 'pointer' }}
+                  onClick={() => selectValue('Sub item 2')}
+                >
+                  Sub item 2
+                </Accordion.Panel>
+                <Accordion.Panel
+                  sx={{ cursor: 'pointer' }}
+                  onClick={() => selectValue('Sub item 3')}
+                >
+                  Sub item 3
+                </Accordion.Panel>
+              </Box>
+            </Accordion.Item>
+          </Accordion>
+        </Text>
+      </Collapse>
+    </Box>
+  );
+}
--- src/layout/AdminHeader.tsx
+++ src/layout/AdminHeader.tsx
@@ -1,6 +1,8 @@
-import { ActionIcon, Box, Header, createStyles } from '@mantine/core';
+import { ActionIcon, Box, Grid, Header, Select, createStyles } from '@mantine/core';
 
 import { IconLogout } from '@tabler/icons-react';
+import { useState } from 'react';
+import { MultiLevelSelect } from '../components/MultiLevelSelect';
 
 interface Props {
   burger?: React.ReactNode;
@@ -13,21 +15,36 @@ const useStyles = createStyles((theme) => ({
     color: theme.black,
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'center',
     justifyContent: 'space-between',
   },
 }));
 
 export function AdminHeader({ burger }: Props) {
   const { classes } = useStyles();
+  const [value, setValue] = useState<string>('2021');
 
   return (
     <Header height={60} withBorder={false} className={classes.header}>
       {burger && burger}
       <Box sx={{ flex: 1 }} />
-      <ActionIcon>
-        <IconLogout size="1.25rem" />
-      </ActionIcon>
+      <Grid align="top">
+        <MultiLevelSelect />
+        <Select
+          ml={10}
+          value={value}
+          size="sm"
+          withinPortal
+          onChange={(v) => v && setValue(v)}
+          data={[
+            { value: '2021', label: '2021' },
+            { value: '2022', label: '2022' },
+            { value: '2023', label: '2023' },
+          ]}
+        />
+        <ActionIcon ml={5} mt={3}>
+          <IconLogout size="1.25rem" />
+        </ActionIcon>
+      </Grid>
     </Header>
   );
 }
