f2126b0c7dbb521105b6e65a6feefcda3051ded7
vitaliy <vitaliy@heliotrope.digital>
2023-08-02 15:28:48 +0300

    login menu added

--- src/navlinks.js
+++ src/navlinks.js
@@ -1,6 +1,7 @@
-import { IconDashboard, IconHome, IconList } from '@tabler/icons-react';
+import { IconDashboard, IconHome, IconList, IconLogin } from '@tabler/icons-react';
 
 export const navLinks = [
+  { label: 'Login', icon: IconLogin, link: '/login' },
   { label: 'Dashboard', icon: IconDashboard, link: '/' },
   { label: 'Home', icon: IconHome, link: '/home' },
   { label: 'Action Items', icon: IconList, link: '/items' },
